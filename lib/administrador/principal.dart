77224c8f666fea7f9426136a89cfd5d3d6e798da
Gespinosa <gespinosa01@icloud.com>
2024-03-31 23:23:44 -0700

    Creando carpetas y pantallas de los actores

---
+++ lib/administrador/principal.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const ScreenAdministrador());
+
+class ScreenAdministrador extends StatelessWidget {
+  const ScreenAdministrador({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
