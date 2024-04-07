5feafdb63a107318fa6e8d5f46e7ca815973d144
jvm750 <jomavazquezme@ittepic.edu.mx>
2024-04-06 21:17:53 -0700

    Diseño del Home de Usuario

--- lib/usuario/principal.dart
+++ lib/usuario/principal.dart
@@ -22,8 +22,80 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: const Text('HOME',style: TextStyle(color: Colors.white),),
+        actions: [
+          IconButton(
+            color: Colors.white,
+            icon: const Icon(Icons.person),
+            onPressed: () {
+              // Acciones del ícono
+            },
+          )
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: 5, // Número de elementos en la lista
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            margin: const EdgeInsets.all(8.0),
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.red,
+                radius: 30, // Ajusta el tamaño como sea necesario
+              ),
+              title: const Text('USER'),
+              subtitle: const Text('Título'),
+              trailing: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: const [
+                  Text('Fecha'),
+                  Text('Hora'),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.black54,
+        selectedItemColor: Colors.white,
+        backgroundColor: Colors.green,
+        type: BottomNavigationBarType.fixed, // Esto es necesario para más de 3 items
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment),
+            label: 'Trámites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.crisis_alert),
+            label: 'Reportes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.payment),
+            label: 'Pagos',
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
