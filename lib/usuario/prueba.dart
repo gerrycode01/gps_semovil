e280ea2dc4d55b83094caccfb4ec6040c8eda560
Gespinosa <gespinosa01@icloud.com>
2024-04-06 22:24:19 -0700

    Probando conexion remota a firebase

---
+++ lib/usuario/prueba.dart
@@ -0,0 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+FirebaseFirestore db = FirebaseFirestore.instance;
+
+Future<List> getPeople() async {
+  List people = [];
+  CollectionReference collectionReferencePeople = db.collection('personas');
+
+  QuerySnapshot queryPeople = await collectionReferencePeople.get();
+  for (var documento in queryPeople.docs) {
+    people.add(documento.data());
+  }
+
+  return people;
+}
