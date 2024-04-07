101ba0d8e3fefb261205312de9ac8bb7d18560a7
jvm750 <jomavazquezme@ittepic.edu.mx>
2024-04-06 21:19:46 -0700

    Diseño 2.0 no hize el push xd

--- lib/usuario/principal.dart
+++ lib/usuario/principal.dart
@@ -44,7 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
         itemCount: 5, // Número de elementos en la lista
         itemBuilder: (BuildContext context, int index) {
           return Card(
-            margin: const EdgeInsets.all(8.0),
+            margin: const EdgeInsets.all(9.0),
             child: ListTile(
               leading: CircleAvatar(
                 backgroundColor: Colors.red,
