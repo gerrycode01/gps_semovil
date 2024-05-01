5ec9301d70656b1c5b9f19571f67c34befd64687
jvm750 <jomavazquezme@ittepic.edu.mx>
2024-05-01 12:00:59 -0700

    diseño de botones y text field

---
+++ lib/diseño.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class Diseno{
+
+  static TextFormField campoTexto(TextEditingController t, String texto){
+    return TextFormField(
+      controller: t,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.orange[100],
+        hintText: texto,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    );
+  }
+  static Widget botonGreen(String texto, void Function() accion){
+    return ElevatedButton(
+      onPressed: () {
+        accion();
+      },
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(Colors.green), // Background color
+        foregroundColor: MaterialStateProperty.all<Color>(Colors.white), // Text color
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
+      child: Text(texto),
+    );
+  }
+  static Widget botonRed(String texto, void Function() accion){
+    return ElevatedButton(
+      onPressed: () {
+        accion();
+      },
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(Colors.red), // Background color
+        foregroundColor: MaterialStateProperty.all<Color>(Colors.white), // Text color
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
+      child: Text(texto),
+    );
+  }
+}
