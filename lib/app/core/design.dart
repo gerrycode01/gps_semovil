2a2cf323345635dfeece846a5197d46d9419bcb4
jvm750 <jomavazquezme@ittepic.edu.mx>
2024-05-05 12:10:52 -0700

    refactorizacion al 75%

--- lib/app/core/design.dart
+++ lib/app/core/design.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Design{
   static TextFormField campoTexto(TextEditingController t, String texto){
@@ -47,6 +48,67 @@ class Design{
         ),
       ),
       child: Text(texto),
+    );
+  }
+  static Widget campoFecha(BuildContext context, TextEditingController controller, String hintText) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.orange[100],
+        hintText: hintText,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide.none,
+        ),
+      ),
+      readOnly: true,
+      onTap: () async {
+        FocusScope.of(context).requestFocus(FocusNode());
+        DateTime? pickedDate = await showDatePicker(
+          context: context,
+          initialDate: DateTime.now(),
+          firstDate: DateTime(1900),
+          lastDate: DateTime.now(),
+        );
+        if (pickedDate != null) {
+          String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate);
+          controller.text = formattedDate;
+        }
+      },
+    );
+  }
+  static Widget campoDropdown<T>({
+    required T value,
+    required List<T> items,
+    required void Function(T?) onChanged,
+    required String hintText,
+  }) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+      decoration: BoxDecoration(
+        color: Colors.orange[100],
+        borderRadius: BorderRadius.circular(30),
+        border: Border.all(color: Colors.transparent),
+      ),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton<T>(
+          value: value,
+          isExpanded: true,
+          icon: const Icon(Icons.arrow_drop_down, color: Colors.black),
+          iconSize: 24,
+          elevation: 16,
+          style: const TextStyle(color: Colors.black, fontSize: 16),
+          onChanged: onChanged,
+          items: items.map<DropdownMenuItem<T>>((T value) {
+            return DropdownMenuItem<T>(
+              value: value,
+              child: Text(value.toString()),
+            );
+          }).toList(),
+          hint: Text(hintText, style: const TextStyle(color: Colors.black)),
+        ),
+      ),
     );
   }
 }
